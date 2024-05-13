da42d47179e482e8c4d6692d100180c9ed81ae48
Abraão Victor <abraaovictor201@gmail.com>
2024-05-13 16:22:55 -0300

    Adicionando classe de dados

--- Cod3rsGrowth.Dominio/Class1.cs
+++ Cod3rsGrowth.Dominio/Class1.cs
@@ -1,7 +1,29 @@
-﻿namespace Cod3rsGrowth.Dominio
-{
-    public class Class1
-    {
+﻿using System.Data;
 
+namespace Cod3rsGrowth.Dominio
+{
+     internal class Cliente
+    {
+        string nome;
+        int id;
+        string cfp;
+        string cnpj;
+        enum tipo
+        {
+            Fisico = 1,
+            Juridico = 2
+        }
+    }
+     internal class Pedido
+    {
+        int clienteid;
+        DateTime data;
+        decimal numeroCartao;
+        enum formaPagamento
+        {
+            Cartao = 1,
+            Pix = 2,
+            Boleto = 3
+        }
     }
 }
