eda93ad379774e0e094e1d90a2f56e603e8af380
Abraão Victor <abraaovictor201@gmail.com>
2024-05-14 10:16:22 -0300

    Separando as classes Cliente e Pedido, e adicionado get e set

---
+++ Cod3rsGrowth.Dominio/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+
+namespace Cod3rsGrowth.Dominio
+{
+     public class Cliente
+    {
+        public string Nome { get; set; }
+        public int Id { get; set; }
+        public string Cfp { get; set; }
+        public string Cnpj { get; set; }
+        public enum Tipo 
+        {
+            Fisico = 1,
+            Juridico = 2
+        }
+    }
+}
---
+++ Cod3rsGrowth.Dominio/Pedido.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cod3rsGrowth.Dominio
+{
+    public class Pedido
+    {
+        public int ClienteId { get; set; }
+        public DateTime Data { get; set; }
+        public decimal NumeroCartao { get; set; }
+        public enum FormaPagamento
+        {
+            Cartao = 1,
+            Pix = 2,
+            Boleto = 3
+        }
+    }
+}
