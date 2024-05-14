ae0dc76110acfd64f881319506ca090374ddaf36
Abraão Victor <abraaovictor201@gmail.com>
2024-05-14 14:31:59 -0300

    Começando a preparar o ambiente de testes

---
+++ Cod3rsGrowth.Testes/ModuloInjecao.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestPlatform.Common.Telemetry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cod3rsGrowth.Testes
+{
+    public interface ICliente
+    {
+        insert Nome();
+        insert Id();
+        insert Cpf();
+        insert Cnpj();
+        insert Tipo();
+    }
+}
---
+++ Cod3rsGrowth.Testes/TesteBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cod3rsGrowth.Testes
+{
+    public class TesteBase : IDisposable
+    {
+        protected ServiceProvider ServiceProvider;
+        MeuServico = new MeuServico();
+        void IDisposable.Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
