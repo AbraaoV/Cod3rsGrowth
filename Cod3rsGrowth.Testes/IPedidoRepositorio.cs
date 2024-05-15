1c0a68a3fdb7c3b035747ef983e70b7b673bef88
Abraão Victor <abraaovictor201@gmail.com>
2024-05-15 12:37:59 -0300

    criado a interface com implemantação no repositorio mock de teste

---
+++ Cod3rsGrowth.Testes/IPedidoRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cod3rsGrowth.Dominio;
+
+namespace Cod3rsGrowth.Testes
+{
+    public interface IPedidoRepositorio
+    {
+        List<Pedido> ObterTodos();
+        Pedido ObterPorId(int id);
+        void Atualizar(int id, Pedido pedido);
+        void Deletar(int id);
+        int Adicionar(Pedido pedido);
+    }
+}
