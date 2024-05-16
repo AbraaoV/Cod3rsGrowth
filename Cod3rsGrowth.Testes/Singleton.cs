2642f07da24bef43f5f34c99d477e6e456b79d6c
Abraão Victor <abraaovictor201@gmail.com>
2024-05-16 09:35:17 -0300

    deletado ClinteSingleton e PedidoSingleton e adcionando a classe singleton vazia

---
+++ Cod3rsGrowth.Testes/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cod3rsGrowth.Dominio;
+
+namespace Cod3rsGrowth.Testes
+{
+    public sealed class Singleton
+    {
+        private static readonly Singleton instance = new Singleton();
+        public List<Singleton> ListaSingleton = new List<Singleton>();
+
+        static Singleton() { }
+
+        private Singleton() { }
+
+        public static Singleton Instance { get { return instance; } }
+    }
+    
+}
