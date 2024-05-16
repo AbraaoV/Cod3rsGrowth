b7cd6383da1fffe79587e7fdaf2cadff3bc3da5f
Abraão Victor <abraaovictor201@gmail.com>
2024-05-16 10:00:29 -0300

    Criador um Singleton para cliente e outra para Pedido

---
+++ Cod3rsGrowth.Testes/TabelaCliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cod3rsGrowth.Dominio;
+
+namespace Cod3rsGrowth.Testes
+{
+    public sealed class TabelaPedido
+    {
+        private static readonly TabelaPedido instance = new TabelaCliente();
+        public List<Cliente> ListaCliente = new List<Cliente>();
+
+
+        static TabelaPedido() { }
+
+        private TabelaPedido() { }
+
+        public static TabelaPedido Instance { get { return instance; } }
+    }
+    
+}
---
+++ Cod3rsGrowth.Testes/TabelaPedido.cs
@@ -0,0 +1,22 @@
+﻿using Cod3rsGrowth.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cod3rsGrowth.Testes
+{
+    public sealed class TabelaPedido
+    {
+        private static readonly TabelaPedido instance = new TabelaPedido();
+        public List<Pedido> ListaCliente = new List<Pedido>();
+
+
+        static TabelaPedido() { }
+
+        private TabelaPedido() { }
+
+        public static TabelaPedido Instance { get { return instance; } }
+    }
+}
