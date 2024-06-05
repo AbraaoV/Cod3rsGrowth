394326ba1118f3e79aaa88d1ccbf515bc267ec83
Abraão Victor <abraaovictor201@gmail.com>
2024-06-05 09:02:18 -0300

    Criado a tabela usando fluentmigrator

---
+++ Cod3rsGrowth.Forms/Migração 20240604123200.cs
@@ -0,0 +1,31 @@
+﻿using FluentMigrator;
+
+namespace Cod3rsGrowth.Forms
+{
+    [Migration(20240604123200)]
+    public class AdicionarTabelas : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Cliente")
+                .WithColumn("Nome").AsString()
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("Cpf").AsString()
+                .WithColumn("Cnpj").AsString()
+                .WithColumn("Tipo").AsInt32();
+
+            Create.Table("Pedido")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("ClienteId").AsInt32().ForeignKey("Cliente", "Id")
+                .WithColumn("Data").AsDateTime()
+                .WithColumn("NumeroCartao").AsString()
+                .WithColumn("Valor").AsDecimal()
+                .WithColumn("FormaPagamento").AsInt32();
+        }
+        public override void Down()
+        {
+            Delete.Table("Cliente");
+            Delete.Table("Pedido");
+        }
+    }
+}
--- Cod3rsGrowth.Forms/Program.cs
+++ Cod3rsGrowth.Forms/Program.cs
@@ -1,4 +1,10 @@
+using System;
 using System.Configuration;
+using System.Linq;
+using FluentMigrator.Runner;
+using FluentMigrator.Runner.Initialization;
+using Microsoft.Extensions.DependencyInjection;
+
 
 namespace Cod3rsGrowth.Forms
 {
@@ -10,11 +16,39 @@ namespace Cod3rsGrowth.Forms
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
-
+            using (var serviceProvider = CreateServices())
+            using (var scope = serviceProvider.CreateScope())
+            {
+                UpdateDatabase(scope.ServiceProvider);
+            }
         }
+        private static ServiceProvider CreateServices()
+        {
+            var appSettings = ConfigurationManager.AppSettings;
+            string result = appSettings["ConnectionString"];
+
+            return new ServiceCollection()
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    .AddSqlServer()
+                    .WithGlobalConnectionString(result)
+                    .ScanIn(typeof(AdicionarTabelas).Assembly).For.Migrations())
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                .BuildServiceProvider(false);
+        }
+
+
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+            runner.MigrateUp();
+        }
+        //To customize application configuration such as set high DPI settings or default font,
+        //see https://aka.ms/applicationconfiguration.
+        //ApplicationConfiguration.Initialize();
+        //Application.Run(new Form1());
+
+        
     }
 }
